79d7d1e70fd05ddcd737a4a55ecc22503d8a91b6
saechaol <33850575+saechaol@users.noreply.github.com>
2021-04-10 14:50:03 -0700

    added registration route

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify, make_response, render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from flask_pymongo import pymongo
+from flask_restful import Resource, Api
 from bson import json_util
+from bson.json_util import dumps
 import requests
 import json
 import config
@@ -22,14 +24,41 @@ def test_get():
 # test get data
 @app.route('/api/test/get/item')
 def test_get_item():
-    documents = config.user_collection.find_one()
-    response = []
-    response.append(documents)
+    documents = dumps(config.sample_collection.find_one({"_id" : "10006546"}))
+    #response = []
+    #response.append(documents)
     # for document in documents:
     #     document['_id'] = str(document['_id'])
     #     response.append(document)
     #     print(document)
-    return json.dumps(response, default=json_util.default)
+    #return json.dumps(response, default=json_util.default)
+    return documents
+
+# post user data
+@app.route('/api/post/register/test', methods=["POST"])
+def test_register_user():
+    config.user_collection.insert_one({'_id': '0', 'username': 'john', 'score': 1000})
+    return jsonify(message="success")
+
+# register user
+@app.route('/api/post/register', methods=["POST"])
+def register_user(): 
+    userId = config.user_collection.count() + 1
+    username = request.args.get('username')
+    password = request.args.get('password')
+    email = request.args.get('email')
+    score = 0
+    config.user_collection.insert_one({'_id': userId, "email": email, "username": username, "password": password, "score": score})
+
+# get user 
+@app.route('/api/get/user/', methods=["GET"])
+def get_user():
+    # if key doesnt exist, return nothing
+    userId = request.args.get('userId')
+    user = config.user_collection.find_one({"_id": userId})
+    return user
+
+
 
 if __name__ == '__main__':
     app.run()
