e414113eac29b5c234d9371359a535b071cb5223
Ninh Le <ninh.le.94@gmail.com>
2021-04-09 23:27:03 -0700

    small fixes for index.js
    sorry

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Router} from "react-router-dom";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
@@ -27,4 +27,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.unregister;
+serviceWorker.unregister();
