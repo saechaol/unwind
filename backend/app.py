d10f725d25026b48ed59b11183414b5c1131fab6
saechaol <33850575+saechaol@users.noreply.github.com>
2021-04-10 01:45:47 -0700

    more python code

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 import requests
 import json
+import config
 
 app = Flask(__name__)
 CORS(app)
@@ -15,3 +16,17 @@ def get_entry():
 @app.route('/api/test/get')
 def test_get():
     return ("GET request successful!\n/api/test/get")
+
+# test get data
+@app.route('/api/test/get/item')
+def test_get_item():
+    documents = config.db.user_collection.find_one()
+    response = []
+    for document in documents:
+        document['_id'] = str(document['_id'])
+        response.append(document)
+        print(document)
+    return json.dumps(response)
+
+if __name__ == '__main__':
+    app.run()
